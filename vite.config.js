e37b4d78ac5601fc7ad5eeb23404263633cbe6fd
张贻娣 <1160184229@qq.com>
2026-08-30 19:49:17 +0800

    Deploy portfolio with GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/yidi-zhangportfolio/' : '/',
   server: {
     port: 3000,
     open: true,
